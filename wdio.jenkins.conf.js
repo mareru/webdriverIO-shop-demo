9166b2ce26eff383f66fc2c7b1a4a5295b884025
BorisArapovic <boris.arapovic@ecx.io>
2019-10-25 10:11:24 +0200

    added jenkins config

--- wdio.jenkins.conf.js
+++ wdio.jenkins.conf.js
@@ -2,18 +2,4 @@ const wdioConfig = require('./wdio.conf.js');
 
 wdioConfig.config.host= 'build-ecx-sitecore';
 
-wdioConfig.config.beforeFeature = [
-    function beforeScenario(uri, feature) {
-        browser.setCookies({name: 'zaleniumMessage', value: feature.name});
-    },
-];
-
-wdioConfig.config.afterStep = [
-            function afterStep(uri, feature, scenario, step, result) {
-                if (result.status === "passed") {
-                    browser.setCookies({name: 'zaleniumTestPassed', value: 'true'});
-                } else {
-                    browser.setCookies({name: 'zaleniumTestPassed', value: 'false'});
-                }
-            },
-        ];
+exports.config = wdioConfig.config;
